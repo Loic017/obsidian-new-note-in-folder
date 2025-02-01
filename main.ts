852061940dceb048311ecc03a1d0c87ff7773ccf
Loic017 <l.loic017@gmail.com>
2025-02-01 20:03:01 +0000

    Add note to a chosen folder

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, SuggestModal, TFolder } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	defaultNoteName: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	defaultNoteName: ''
 }
 
 export default class MyPlugin extends Plugin {
@@ -17,51 +17,19 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'Create note in folder', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'open-create-note-in-folder',
+			name: 'Open menu to create note in a specific folder',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				new FindFolderModal(this.app, this).open();
 			}
 		});
 
@@ -91,21 +59,76 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class FindFolderModal extends SuggestModal<any> {
+	private plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
 		super(app);
+		this.plugin = plugin;
+	}
+
+	getSuggestions(query: string): TFolder[] {
+		return this.app.vault.getAllFolders();
+	}
+	renderSuggestion(folder: TFolder, el: HTMLElement): void {
+		el.createEl("div", { text: folder.path });
+	}
+	onChooseSuggestion(folder: TFolder, evt: MouseEvent | KeyboardEvent): void {
+		new CreateNoteModal(this.app, folder, this.plugin).open();
+	}
+}
+
+class CreateNoteModal extends Modal {
+	folder: TFolder;
+	private note_name: HTMLInputElement;
+	private plugin: MyPlugin;
+
+	constructor(app: App, folder: TFolder, plugin: MyPlugin) {
+		super(app);
+		this.folder = folder;
+		this.plugin = plugin;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+
+		this.note_name = contentEl.createEl('input', {
+			attr: {
+				type: 'text',
+				placeholder: 'Enter note name'
+			},
+			cls: 'mod-cta'
+		});
+
+		const createNoteButton = contentEl.createEl('button', {
+			text: 'Create Note',
+			cls: 'submit-button'
+		});
+
+		createNoteButton.onclick = () => {
+			const fileName = this.note_name.value;
+			if (fileName) {
+				this.app.vault.create(this.folder.path + '/' + fileName + '.md', '');
+				this.close();
+			} else {
+				this.app.vault.create(this.folder.path + '/' + this.plugin.settings.defaultNoteName + '.md', '');
+				this.close();
+			}
+		};
+
+		this.note_name.addEventListener('keypress', (e) => {
+			if (e.key === 'Enter') {
+				createNoteButton.click();
+			}
+		});
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
+
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
@@ -116,18 +139,21 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
+		containerEl.createEl('h1', { text: 'New Note in Folder Plugin' });
+		containerEl.createEl('p', { text: 'To fix the very tedious problem of creating a note in a folder without using the mouse and changing to the sidebar explorer.' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Default Note Name')
+			.setDesc('The default name for a note. Used when the user does not provide a name.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('untitiled')
+				.setValue(this.plugin.settings.defaultNoteName)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.defaultNoteName = value;
 					await this.plugin.saveSettings();
 				}));
 	}
